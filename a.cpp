7804557d105a5204f750026423442092f67047b5
jason taekyun Kim <saurus234@gmail.com>
2022-05-01 17:25:26 +0900

    initial setting

---
+++ a.cpp
@@ -0,0 +1,11 @@
+//기본 C++ 실행방법
+#include <bits/stdc++.h>
+using namespace std; int main() {
+    cout << 1 << "\n";
+return 0; }
+
+//g++ -std=c++14 -Wall a.cpp -o test.out
+//a.cpp파일을 엄격하게(-wall) C++ 14버전으로 컴파일해서 test.out이라는 파일을 
+//만든다(-o test.out)는 의미
+
+//./test.out 명령어로 실행!
